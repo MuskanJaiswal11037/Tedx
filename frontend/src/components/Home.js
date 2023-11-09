66deb3ce1b71394c2c0fd15e93493c045340e710
MuskanJaiswal11037 <102662949+MuskanJaiswal11037@users.noreply.github.com>
2023-11-10 00:58:11 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -19,22 +19,27 @@ return(
 </header>
 <div className="container">
     <div className="Tedx-Main1">
+    <form method="POST" >
+        <input name="Name" type="text" placeholder="Name" required><br>
+        <input name="Email" type="email" placeholder="Email" required><br>
+        <button type="submit">Send</button>
+    </form> 
     </div>
-    <div className="Tedx-Main2">
-        <h3>IDEAS WORTH SPREADING</h3>
-        <br/>
-        <p>
-            This is a platform which provides an atmosphere for thought provoking discussions
-            and a critical way of looking and consequently tackling issues and questions at all levels. 
-            Here, you experience real, jaw-dropping stories which continues to inspire people to reach their potential 
-            and  help them break their limits and go beyond.
-        </p>      
-    </div> 
-    <div className="Tedx-Main3">
-        <img className="img3" src="https://cdn.flipit.money/img/flips/ekKkekgtEeCxFnIjvSJCojB5XdZAh7KetEF4lxOD.jpeg" alt="img"/>   
-        <img className="img3" src="https://iithadmissionsblog.files.wordpress.com/2016/06/img_0310.jpg?w=1200" alt="img"/>    
-    </div>
-</div>
+//     <div className="Tedx-Main2">
+//         <h3>IDEAS WORTH SPREADING</h3>
+//         <br/>
+//         <p>
+//             This is a platform which provides an atmosphere for thought provoking discussions
+//             and a critical way of looking and consequently tackling issues and questions at all levels. 
+//             Here, you experience real, jaw-dropping stories which continues to inspire people to reach their potential 
+//             and  help them break their limits and go beyond.
+//         </p>      
+//     </div> 
+//     <div className="Tedx-Main3">
+//         <img className="img3" src="https://cdn.flipit.money/img/flips/ekKkekgtEeCxFnIjvSJCojB5XdZAh7KetEF4lxOD.jpeg" alt="img"/>   
+//         <img className="img3" src="https://iithadmissionsblog.files.wordpress.com/2016/06/img_0310.jpg?w=1200" alt="img"/>    
+//     </div>
+ </div>
 
 
 </body>
